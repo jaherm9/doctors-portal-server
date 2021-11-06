f5b3d03f3bb0f723a85526a535994423a82e40ef
shaka <jaherm9@gmail.com>
2021-11-06 19:22:36 +0600

    70-9 Module Summary And Database Connection

--- index.js
+++ index.js
@@ -7,13 +7,26 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+// connect to database
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8ztnx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-console.log(uri);
+//CRUD Operatoin
+
+async function run() {
+  try {
+      await client.connect();
+      console.log('database conntected successfully');
+  } 
+  finally {
+      // await client.close();
+  }
+}
+
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello Doctors portal!");
